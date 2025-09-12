cf402ecc4a13fa80ed3817dc2feb2bb5e7c1e824
Paras Narkhade <parassnarkhade@gmail.com>
2025-09-12 18:43:49 +0530

    Add files via upload

---
+++ RightTrianglePattern.java
@@ -0,0 +1,14 @@
+
+ public class RightTrianglePattern {
+    
+    public static void main(String[] args) {
+        
+        int[] marks = {100, 32, 66, 44, 66, 33, 22, 89 };
+
+        for(int i=0; i<marks.length; i++){
+            if(marks[i]<35)
+              System.out.println(marks[i] + " ");
+        }
+        
+    }
+ }
